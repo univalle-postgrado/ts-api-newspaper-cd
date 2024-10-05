f1082155f008561da3ada941385fdcc31f286051
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-10-05 17:29:32 -0400

    Fix module User controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,20 +1,20 @@
 import * as express from "express";
 import { Request, Response } from "express"
-import { UserController } from "../controllers/UserController"
+// import { UserController } from "../controllers/UserController"
 import { AppDataSource } from "../data-source"
 import { User } from "../entity/User"
 
 const Router = express.Router();
 
 // registramos rutas routes
-Router.get("/users", UserController.getAll)
+// Router.get("/users", UserController.getAll)
 
-Router.get("/users/:id", async function (req: Request, res: Response) {
-    const results = await AppDataSource.getRepository(User).findOneBy({
-        id: req.params.id,
-    })
-    return res.send(results)
-})
+// Router.get("/users/:id", async function (req: Request, res: Response) {
+//     const results = await AppDataSource.getRepository(User).findOneBy({
+//         id: req.params.id,
+//     })
+//     return res.send(results)
+// })
 
 // Router.post("/users", async function (req: Request, res: Response) {
 //     const user = await AppDataSource.getRepository(User).create(req.body)
