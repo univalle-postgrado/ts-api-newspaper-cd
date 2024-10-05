715675de84121d04b348916f0811b4036f452fe1
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-10-05 18:09:16 -0400

    Fix deploy in AWS

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,14 +5,19 @@ import { Category } from "./entity/Category";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "ls-be4e84dc3bde666b72d0069d6199a517d5b6f83d.c2gjf8cot7pl.us-east-1.rds.amazonaws.com",
     port: 5432,
-    username: "dbadmin",
-    password: "",
+    username: "dbmasteruser",
+    password: "37?}!$$%<#>)m[kI$n[h2DS$=K31Lbn*",
     database: "ts_bdapinewspaper",
     synchronize: true,
     logging: false,
     entities: [Category],
     migrations: [__dirname + "/migration/*.ts"],
     subscribers: [],
+    extra: {
+      "ssl": {
+         "rejectUnauthorized": false
+      }
+    }
 })
